b366da91ea3b2695e42be102dca98196c6203248
hellohy <hitcloudy@gmail.com>
2018-06-08 01:30:22 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,36 @@ module.exports = {
   description: 'front-end-tutorial',
   base: '/enduring-heart/',
   themeConfig: {
-    sidebar: genSidebarConfig(''),
-    sidebarDepth: 2
+    sidebar: {
+      '/': genSidebarConfig(''),
+      '/curated-tutorial/': [
+        {
+          title: '基础教程',
+          collapsable: false,
+          children: [
+            '',
+            'basic-css',
+            'basic-html',
+            'basic-javascript'
+          ]
+        }
+      ]
+    },
+    sidebarDepth: 2,
+    nav: [
+      {
+        text: '基础教程',
+        link: '/curated-tutorial/'
+      },
+      {
+        text: '分享会',
+        link: '/share/'
+      },
+      {
+        text: '翻译',
+        link: '/translation/'
+      }
+    ]
   }
 }
 
